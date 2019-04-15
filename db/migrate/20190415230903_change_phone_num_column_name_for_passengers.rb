53fdbb51433de458964282dc4e2798fba4991982
Riyo <riyo.perry@gmail.com>
2019-04-15 16:21:44 -0700

    changed phone number column name

---
+++ db/migrate/20190415230903_change_phone_num_column_name_for_passengers.rb
@@ -0,0 +1,5 @@
+class ChangePhoneNumColumnNameForPassengers < ActiveRecord::Migration[5.2]
+  def change
+    rename_column :passengers, :phone_number, :phone_num
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_15_225552) do
+ActiveRecord::Schema.define(version: 2019_04_15_230903) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,7 +28,7 @@ ActiveRecord::Schema.define(version: 2019_04_15_225552) do
 
   create_table "passengers", force: :cascade do |t|
     t.string "name"
-    t.string "phone_number"
+    t.string "phone_num"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "trip_id"
