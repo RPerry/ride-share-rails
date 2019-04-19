cdcecb4bfa5cc732b67da8b86e5a6f0bdaf76040
Pauline Sauget <paulinevsauget@gmail.com>
2019-04-18 17:24:00 -0700

    Update tests & add custom routes for destroy action

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,15 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'homepages#index'
 
-  resources :drivers
+  resources :drivers, except: :delete
+  resources :passengers, except: :delete
   resources :trips
-  resources :passengers
 
   resources :passengers do
     resources :trips, only: [:index, :new, :create]
   end
 
   post '/drivers/:id/available', to: 'drivers#available', as: "driver_availability"
+  post '/drivers/:id/destroy', to: 'drivers#destroy', as: "destroy_driver"
+  post '/passengers/:id/destroy', to: 'passengers#destroy', as: "destroy_passenger"
 end
