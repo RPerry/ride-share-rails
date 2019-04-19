3a258122c65c6cdb75aaaffe9a9c1c4c6e8850a2
Pauline Sauget <paulinevsauget@gmail.com>
2019-04-19 15:56:40 -0700

    DRY up routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'homepages#index'
 
-  resources :drivers, except: :delete
-  resources :passengers, except: :delete
+  resources :drivers
   resources :trips
 
   resources :passengers do
@@ -11,6 +10,6 @@ Rails.application.routes.draw do
   end
 
   post '/drivers/:id/available', to: 'drivers#available', as: "driver_availability"
-  post '/drivers/:id/destroy', to: 'drivers#destroy', as: "destroy_driver"
-  post '/passengers/:id/destroy', to: 'passengers#destroy', as: "destroy_passenger"
+  # post '/drivers/:id/destroy', to: 'drivers#destroy', as: "destroy_driver"
+  # post '/passengers/:id/destroy', to: 'passengers#destroy', as: "destroy_passenger"
 end
